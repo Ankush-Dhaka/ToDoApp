87d981870e387338c0690fa7b3b4374f8becfe65
Ankush-Dhaka <163640803+Ankush-Dhaka@users.noreply.github.com>
2024-04-16 01:18:09 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+let item = document.querySelector('#item');
+let todobox = document.querySelector('#to-do-box');
+
+item.addEventListener(
+    "keyup",function(event){
+        if(event.key=="Enter"){
+            addToDo(this.value);
+            this.value="";
+        }
+    }    
+)
+let addToDo = (item)=>{
+    let listItem = document.createElement("li");
+    listItem.innerHTML=`
+    ${item}
+    <i class="fas fa-times"></i>`;
+     
+    listItem.addEventListener("click",function(){
+        this.classList.toggle("done");
+    });
+    listItem.querySelector("i").addEventListener("click",function(){listItem.remove()})
+    todobox.appendChild(listItem)
+}
